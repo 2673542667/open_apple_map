ec8f47ddb6f6fe448582206a773bab16390a4a57
HuangYuLin <2673542667@qq.com>
2020-03-10 09:33:06 +0800

    暂存

--- example/android/app/src/main/java/map/apple/com/open_apple_map_example/MainActivity.java
+++ example/android/app/src/main/java/map/apple/com/open_apple_map_example/MainActivity.java
@@ -1,6 +1,6 @@
 package map.apple.com.open_apple_map_example;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
